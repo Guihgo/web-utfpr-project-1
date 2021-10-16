82539ce971dbc17e3d6642570e44b2f0de68f4a0
fabiolk <fabioos8@hotmail.com>
2021-10-16 16:38:45 -0300

    config index file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let http = require('http')
-let express = require('express')
+let express = require('express');
+const path = require('path');
 let app = express()
 let server = http.createServer(app)
 
@@ -7,8 +8,11 @@ let port = process.argv[2] || 80;
 server.listen(port)
 console.log('Servidor http rodando na porta', port)
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Servidor Web Rodando na porta ' + port)
+    res.sendFile(path.join(__dirname, 'index.html'))
+    // res.send('Servidor Web Rodando na porta ' + port)
 })
 
 app.get('/teste', (req, res) => {
